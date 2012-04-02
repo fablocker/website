af33f1c82b75efc5b8c936fda6774a6c246389da
Bogdan Varlamov <bogdanvarlamov@gmail.com>
2012-04-02 10:03:19 -0400

    updating rsvp display

--- src/rsvp/views.py
+++ src/rsvp/views.py
@@ -3,16 +3,29 @@ from .models import Attendant
 from django.shortcuts import render
 
 def rsvp(request, event):
+    error = None
+    saved = False
     if request.method == 'POST':
         form = EventRsvpForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
-            attendant = Attendant()
-            attendant.event = event
-            attendant.email = email
-            attendant.save()            
+            form_email = form.cleaned_data['email']
+            if(Attendant.objects.filter(email = form_email, event = event).exists()):
+                    error = "Thanks! This e-mail has already been registered : ) " \
+                        "You can register another though..."
+            else:
+                    attendant = Attendant()
+                    attendant.event = event
+                    attendant.email = form_email
+                    attendant.save()
+                    saved = True
     else:
-        form = EventRsvpForm()
+        data = {}
+        if request.user.is_authenticated():
+            usermail = request.user.email
+            if(usermail):
+                data = { "email" : usermail }
+                form = EventRsvpForm(data)
+        else:
+            form = EventRsvpForm()
     return render(request, 'rsvp/event_detail.html',
-           {'form': form, 'event' : event,},)
-        # Always return an HttpResponseRedirect after successfully dealing
+           {'form': form, 'event' : event, 'error': error, 'saved': saved},)
